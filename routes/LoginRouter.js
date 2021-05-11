51c3f86b1e88f3e93ebc914cf1681af80153757d
Richard Osmar Leon Ingaruca <jrichardsz.java@gmail.com>
2021-05-11 18:52:55 -0500

    done: captcha, github security, improve security, force brute attack protection for api (#29)

--- routes/LoginRouter.js
+++ routes/LoginRouter.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+var captchapng = require('captchapng');
 
 function LoginRouter(expressInstance) {
 
@@ -9,7 +10,7 @@ function LoginRouter(expressInstance) {
 
     // no login in session
     if (!req.session.loginInformation || req.session.loginInformation.user == "") {
-      res.render('login.hbs', {});
+      _this.goToLoginPage(req, res, {})
       return;
     }
 
@@ -32,55 +33,66 @@ function LoginRouter(expressInstance) {
 
     authenticationRepository.findOneByUserName(req.body.user.toLowerCase(), function(err, userInformation) {
 
-      if (err) {
+      if (err || userInformation == null) {
         logger.info(err);
-        res.render('login.hbs', {
-          error_message: "Incorrect user or password."
-        });
+        req.session.error_message = "Incorrect user, password or captcha.";
+        res.redirect("/login")
+        return;
       }
 
       bcrypt.compare(req.body.password, userInformation.password, function(compareErr, compareResult) {
 
         if (compareErr) {
+          logger.info("Failed at compare password with bcrypt");
           logger.info(compareErr);
-          res.render('login.hbs', {
-            error_message: "Internal error."
-          });
+          req.session.error_message = "Internal error.";
+          res.redirect("/login");
+          return;
         }
 
-        if (compareResult) {
-          req.session.loginInformation = {
-            "user": userInformation.username,
-            "role": userInformation.role
-          };
-          req.session.save();
-          res.redirect('/');
-        } else {
+        if(!compareResult){
           logger.info("Password incorrect for user:" + req.body.user);
-          res.render('login.hbs', {
-            error_message: "Incorrect user or password."
-          });
+          req.session.error_message = "Incorrect user, password or captcha.";
+          res.redirect("/login")
+          return;
         }
-      });
 
-      // if (userInformation[0].password == req.body.password) {
-      //
-      //   req.session.loginInformation = {
-      //     "user": userInformation[0].user,
-      //     "role": userInformation[0].role
-      //   };
-      //
-      //   req.session.save();
-      //
-      //   res.redirect('/');
-      // } else {
-      //   logger.info("Password incorrect for user:" + req.body.user);
-      //   res.render('login.hbs', {
-      //     error_message: "Incorrect user or password."
-      //   });
-      // }
+        if (new Number(req.body.captcha) != req.session.captchaSecret) {
+          logger.info("Captcha incorrect for user:" + req.body.user);
+          req.session.error_message = "Incorrect user, password or captcha.";
+          res.redirect("/login")
+          return;
+        }
+
+        req.session.loginInformation = {
+          "user": userInformation.username,
+          "role": userInformation.role
+        };
+        req.session.save();
+        res.redirect('/');
+      });
     });
   });
+
+  this.createCaptchaImageBase64 = function(secretCaptcha) {
+    var p = new captchapng(100,30,secretCaptcha); // width,height,numeric captcha
+    p.color(115, 95, 197, 100);  // First color: background (red, green, blue, alpha)
+    p.color(30, 104, 21, 255); // Second color: paint (red, green, blue, alpha)
+    var img = p.getBase64();
+    var imgbase64 = new Buffer(img,'base64');
+    return Buffer.from(imgbase64).toString('base64');   ;
+  }
+
+  this.goToLoginPage = function(req, res, redirectAttributes) {
+    var captchaSecret = parseInt(Math.random()*90000000+10000000);
+    var captchaImageBase64 = _this.createCaptchaImageBase64(captchaSecret);
+    req.session.captchaSecret = captchaSecret;
+    redirectAttributes.captcha = captchaImageBase64;
+    redirectAttributes.success_message = req.session.success_message;
+    redirectAttributes.warning_message = req.session.warning_message;
+    redirectAttributes.error_message = req.session.error_message;
+    res.render('login.hbs', redirectAttributes);
+  }
 
 }
 
